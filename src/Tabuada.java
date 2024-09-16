a41a4f2b3fb24bc89dac4803e20d6045d7a110a5
EdivoneAraujo <edhyvone2015@gmail.com>
2024-09-15 23:07:58 -0300

    Complete

--- src/Tabuada.java
+++ src/Tabuada.java
@@ -1,15 +1,58 @@
-//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
-// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
-public class Main {
-    public static void main(String[] args) {
-        //TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
-        // to see how IntelliJ IDEA suggests fixing it.
-        System.out.printf("Hello and welcome!");
+import java.util.Scanner;
 
-        for (int i = 1; i <= 5; i++) {
-            //TIP Press <shortcut actionId="Debug"/> to start debugging your code. We have set one <icon src="AllIcons.Debugger.Db_set_breakpoint"/> breakpoint
-            // for you, but you can always add more by pressing <shortcut actionId="ToggleLineBreakpoint"/>.
-            System.out.println("i = " + i);
+public class Tabuada {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.println("""
+                Bem vindo a Tabuada
+                1. Soma
+                2.Subtração
+                3.Multiplicação
+                4.Divisão
+                0.Sair
+                """);
+        System.out.println("Escolha um operador: ");
+        int operador = scanner.nextInt();
+        System.out.println("Digite um número: ");
+        int numero = scanner.nextInt();
+
+        switch (operador) {
+            case 1: //soma
+                for (int i = 1; i <= 10; i++) {
+                    System.out.println(numero + " + " + i + " = " + (numero + i));
+                }
+                break;
+
+            case 2: //Subtração
+                for (int i = 1; i <= 10; i++) {
+                    System.out.println(numero + " - " + i + " = " + (numero - i));
+                }
+                break;
+
+            case 3: //Multiplicação
+                for (int i = 1; i <= 10; i++) {
+                    System.out.println(numero + " x " + i + " = " + (numero * i));
+                }
+                break;
+
+            case 4: //Divisão
+                for (int i = 1; i <= 10; i++) {
+                    if (i == 0) {
+                        System.out.println("Divisão por zero não é permitida.");
+                    } else {
+                        System.out.println(numero + " / " + i + " = " + ((double) numero / i));
+                    }
+                    break;
+                }
+            case 0: //Sair
+                System.out.println("Programa encerrado");
+                break;
+
+            default:
+                System.out.println("Operação inválida");
+                break;
         }
+        scanner.close();
     }
 }
